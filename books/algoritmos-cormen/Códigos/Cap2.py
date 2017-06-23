dc32a4b9e954573684a674060c289894b4ac7159
Eudes Lemos <eudesllemos@gmail.com>
2017-06-23 08:29:58 -0300

    Update code format from cormen, Cap2

--- books/algoritmos-cormen/Códigos/Cap2.py
+++ books/algoritmos-cormen/Códigos/Cap2.py
@@ -1,48 +1,48 @@
 def e211(a):
-    for i in range(1, len(a)):
-        chave = a[i]
-        posicao = i - 1
+  for i in range(1, len(a)):
+    chave = a[i]
+    posicao = i - 1
 
-        while posicao >= 0 and a[posicao] > chave:
-            a[posicao + 1] = a[posicao]
-            a[posicao] = chave
-            posicao = posicao - 1
+    while posicao >= 0 and a[posicao] > chave:
+      a[posicao + 1] = a[posicao]
+      a[posicao] = chave
+      posicao = posicao - 1
 
-    return a
+  return a
 
 
 def e212(a):
-    for i in range(1, len(a)):
-        chave = a[i]
-        posicao = i - 1
+  for i in range(1, len(a)):
+    chave = a[i]
+    posicao = i - 1
 
-        while posicao >= 0 and a[posicao] < chave:
-            a[posicao + 1] = a[posicao]
-            a[posicao] = chave
-            posicao = posicao - 1
+    while posicao >= 0 and a[posicao] < chave:
+      a[posicao + 1] = a[posicao]
+      a[posicao] = chave
+      posicao = posicao - 1
 
-    return a
+  return a
 
 
 def e213(a, v):
-    r = "NIL"
-    for i in range(len(a)):
-        if a[i] == v:
-            r = i
+  r = "NIL"
+  for i in range(len(a)):
+    if a[i] == v:
+      r = i
 
-    return r
+  return r
 
 
 def e214(a, b):
-    n = len(a)
-    c = [0] * (n + 1)
+  n = len(a)
+  c = [0] * (n + 1)
 
-    for i in range(n - 1, -1, -1):
-        sum_ = a[i] + b[i] + c[i + 1]
-        c[i] = int(sum_ / 2)
-        c[i + 1] = int(sum_ % 2)
+  for i in range(n - 1, -1, -1):
+    sum_ = a[i] + b[i] + c[i + 1]
+    c[i] = int(sum_ / 2)
+    c[i + 1] = int(sum_ % 2)
 
-    return c
+  return c
 
 a = [31, 41, 59, 26, 41, 58]
 print("Valor Origem            :", a)
