c1a29121be44c3423c63ff1f285a99b5946e7205
Eudes Lemos <eudesllemos@gmail.com>
2018-02-22 23:20:31 -0300

    add level 04 of javascript road trip 3 course

---
+++ courses/javascript-road-trip-3/level 04 - Ocean of Objects/02 - Accessing Objects I.js
@@ -0,0 +1,6 @@
+var vehicle1 = {type: "Motorboat", capacity: 6, storedAt: "Ammunition Depot"};
+var vehicle2 = {type: "Jet Ski", capacity: 1, storedAt: "Reef Dock"};
+var vehicle3 = {type: "Submarine", capacity: 8, storedAt: "Underwater Outpost"};
+
+// log the property value to the console
+console.log(vehicle1.capacity);
