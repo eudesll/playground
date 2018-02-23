2a7c01de5784eb1d2d614c452c976596fbafcb03
Eudes Lemos <eudesllemos@gmail.com>
2018-02-22 23:18:23 -0300

    add level 02 of javascript road trip 1 course

---
+++ courses/javascript-road-trip-1/level 02 - Variable Valley/Challenges.js
@@ -0,0 +1,31 @@
+// Variable I + Variable II
+var age = 10
+
+var ageIn5Years = age + 5
+
+// Variable III + Variable IV + Variable Concatenation
+var firstName = "John"
+var lastName = "Doe"
+
+var fullName = firstName + " " + lastName
+
+// Complex Variable
+var index14 = "What can we do? Keep going!".charAt(14)
+
+// Length
+setence.length
+
+// Train Passengers + Train Capacity
+var numPassengers = 0
+numPassengers += 3
+
+var countMessage = "Attention: There are now " + numPassengers +" passengers on the train!"
+
+var trainCapacity = 60
+var occupancy = numPassengers + " passengers present, with " + (trainCapacity - numPassengers) + " seats remaining."
+
+// Sum
+var twenty20 = 3 + 10 + 7
+
+// String Concatenation
+twenty20 = "Never look back." + "To infinity... and beyond!" + "Adventure is out there." + "Just keep swimming."
