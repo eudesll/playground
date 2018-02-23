030ae7154a6b0563dbe3385ca2d4fea93d96d7ae
Eudes Lemos <eudesllemos@gmail.com>
2018-02-22 23:18:05 -0300

    start javascript road trip 1 course

---
+++ courses/javascript-road-trip-1/level 01 - The Cliffs of Value/Challenges.js
@@ -0,0 +1,21 @@
+// Basic Operations
+2 + 6 * 6 - 2 / 1
+// Building Expressions
+2 * (3 + 4)
+// The Modulus Operator
+10 % 6
+// A Boolean Expression
+1 === 2
+// Another Boolean Expression
+1 !== 2
+
+// Building Strings
+name = "Eudes Lemos"
+// Special Characters in Strings
+"Quoth the raven:\n\t\"Nevermore!\""
+// Booleans with Strings
+"a" == "a"
+// Concatenating a Mystery String
+"eight " + 5 + " six " + "two " + 3 + " " + 7
+// Accessing a String's Length
+"111111111111111111111111111111333".length
