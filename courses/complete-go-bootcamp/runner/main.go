3016ff257559db783aa953b08673ad77711be9b4
Eudes Lemos <eudesllemos@gmail.com>
2019-05-07 16:33:02 -0300

    Refact to print the correct question

--- courses/complete-go-bootcamp/runner/main.go
+++ courses/complete-go-bootcamp/runner/main.go
@@ -1,12 +1,28 @@
 package runner
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+	"strings"
+)
 
 //RunSolutions can run functions that solves the course questions
 func RunSolutions(callbacks []func()) {
 	for i := range callbacks {
-		fmt.Println("Solution", i)
+		name := getFuncName(callbacks[i])
+
+		fmt.Println("Solution", name)
 		callbacks[i]()
 		fmt.Println()
 	}
+}
+
+func getFuncName(fn func()) string {
+	functionName := runtime.FuncForPC(reflect.ValueOf(fn).Pointer()).Name()
+
+	names := strings.Split(functionName, ".")
+	name := names[len(names)-1]
+
+	return name
 }
