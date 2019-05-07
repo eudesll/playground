a70b5438c130ca8e2fa9e5b33a68c39070de37a6
Eudes Lemos <eudesllemos@gmail.com>
2019-05-07 16:16:17 -0300

    Add section 4 exercises solutions

---
+++ courses/complete-go-bootcamp/04-statements-expressions-comments/q1.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Shy Semicolons
+//
+//  1. Try to type your statements by separating them using
+//     semicolons
+//
+//  2. Observe how Go fixes them for you
+//
+// ---------------------------------------------------------
+
+func q1() {
+	//fmt.Println("inanc"); fmt.Println("lina"); fmt.Println("ebru");
+	fmt.Println("inanc")
+	fmt.Println("lina")
+	fmt.Println("ebru")
+}
