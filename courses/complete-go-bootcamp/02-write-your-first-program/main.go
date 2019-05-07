40489846f08ae0c0ea9066c6844f9b21388c99ac
Eudes Lemos <eudesllemos@gmail.com>
2019-05-07 16:02:08 -0300

    Refact section 2 exercises

--- courses/complete-go-bootcamp/02-write-your-first-program/main.go
+++ courses/complete-go-bootcamp/02-write-your-first-program/main.go
@@ -1,21 +1,8 @@
 package main
 
-import "fmt"
-
-type fn func()
+import r "../runner"
 
 func main() {
-	runSolutions()
-}
-
-func runSolutions() {
-	runSolution("1", q1)
-	runSolution("4", q4)
-	runSolution("5", q5)
-}
-
-func runSolution(id string, callback fn) {
-	fmt.Println("Solution", id)
-	callback()
-	fmt.Println()
+	questions := []func(){q1, q4, q5}
+	r.RunSolutions(questions)
 }
