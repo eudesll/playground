42b2a7b112674cb1db184549c8cc3a102340a8bc
Eudes Lemos <eudesllemos@gmail.com>
2019-09-14 00:09:50 -0300

    Solve Birthday Cake challenge from HackerRank

---
+++ problem-solving/hacker-rank/algorithms/warmup/Birthday Cake Candles.go
@@ -0,0 +1,15 @@
+// Complete the birthdayCakeCandles function below.
+func birthdayCakeCandles(ar []int32) int32 {
+	numMap := map[int32]int32{}
+	big := int32(0)
+
+	for _, v := range ar {
+		if v > big {
+			big = v
+		}
+
+		numMap[v]++
+	}
+
+	return numMap[big]
+}
